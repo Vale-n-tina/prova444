c5c6fed682abd5b3add2b4e5f564cd168c5ba1c5
Vale-n-tina <valentia.grg@gmail.com>
2025-02-20 11:55:48 +0100

    Initial commit

---
+++ src/test/java/com/example/prova444/Prova444ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prova444;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prova444ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
